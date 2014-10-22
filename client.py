7f838442637eb3bf462b668ff31287797a4a2220
Alberto <alberto@ubuntu.(none)>
2014-10-22 10:46:46 +0200

    cliente sip

--- client.py
+++ client.py
@@ -13,7 +13,7 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
-REGISTRER= upper(sys.argv[3])
+REGISTRER = sys.argv[3].upper()
 LINE = ' '.join(sys.argv[4:])
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,7 +21,7 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 print "Enviando: " + LINE
-my_socket.send(REGISTER + "sip:" + LINE + "SIP 2.0" + '\r\n' + '\r\n')
+my_socket.send(REGISTRER + "sip:" + LINE + "SIP 2.0" + '\r\n' + '\r\n')
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
