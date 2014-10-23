ffa2b098ed5bd5173eed434c408d5f7205c59cfc
alberto pavo blas <apavo@iota20.aulas.gsyc.es>
2014-10-23 14:19:01 +0200

    servidor sip direcciones

--- server.py
+++ server.py
@@ -12,31 +12,34 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
+   
+    diccionario={}
     
-    def procesarmensaje(self,line,diccionario):
+    def procesarmensaje(self,line):
        mensaje = line.split(" ")
        if mensaje[0] == "REGISTRER":
             direccion= mensaje[1].split(":")
-            diccionario[direccion[-1]]= self.client_address[0]
+            self.diccionario[direccion[-1]]= self.client_address[0]
             self.wfile.write("SIP/2.0 200 OK" + '\r\n' + '\r\n')
             
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write("Hemos recibido tu peticion" + '\r\n')
-        self.diccionario={}
+       
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             print self.client_address[0] + " " + str(self.client_address[1])
             line = self.rfile.read()
-            self.procesarmensaje(line,self.diccionario)
+            self.procesarmensaje(line)
             print "El cliente nos manda " + line
+            print self.diccionario
             if not line:
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    HOST,PORT="localhost",int(sys.argv[1])
-    serv = SocketServer.UDPServer((HOST,PORT ), SIPRegisterHandler)
+    HOST,PORT="",int(sys.argv[1])
+    serv = SocketServer.UDPServer((HOST,PORT), SIPRegisterHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
