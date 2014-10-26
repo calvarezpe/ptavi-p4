aaa1741a581f9e29dc10dd69010ef018f64cf358
Christian Alvarez <christian@christian-Aspire-5536.(none)>
2014-10-26 15:14:13 +0100

    Mod: Ejercicio 7 - Client SIP- Campo Expired

--- client.py
+++ client.py
@@ -8,30 +8,34 @@ import socket
 import sys
 
 lista = sys.argv
-# Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = lista[1]
-PORT = int(lista[2])
+if len(lista) == 6:
+    # Cliente UDP simple.
+    # Dirección IP del servidor.
+    SERVER = lista[1]
+    PORT = int(lista[2])
 
-# Contenido que vamos a enviar
-if lista[3] == "register":
-	LINE = lista[3].upper() + " sip:" + lista[4] + " SIP/2.0 \r\n\r\n"
+    # Contenido que vamos a enviar
+    if lista[3] == "register":
+        LINE = lista[3].upper() + " sip:" + lista[4] + " SIP/2.0 \r\n"
+        LINE = LINE + "Expires: " + lista[5] + "\r\n\r\n"
+    else:
+        LINE = " ".join(lista[3:])
+
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
+
+    print "Enviando: " + LINE
+    my_socket.send(LINE) #!!! line + \r\n ???
+    data = my_socket.recv(1024)
+
+    print 'Recibido -- ', data
+    print "Terminando socket..."
+
+    # Cerramos todo
+    my_socket.close()
+    print "Fin."
 else:
-	LINE = " ".join(lista[3:])
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
-
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
-
-print 'Recibido -- ', data
-print "Terminando socket..."
-
-# Cerramos todo
-my_socket.close()
-print "Fin."
+    print "Usage: client.py ip puerto register sip_address expires_value"
