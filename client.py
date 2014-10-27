fd8eae3984f3a1db1c81e8cfab7b6f8c4a42c270
abel vargas vera <avera@iota24.aulas.gsyc.es>
2014-10-27 16:37:57 +0100

    server.py

--- client.py
+++ client.py
@@ -26,23 +26,26 @@ try:
     # Valor de Expires en segundos
     EXPIRES = listarg[5]
 
-    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
-
-    print "Enviando: " + TYPE.upper() + " sip:" + LINE + " SIP/1.0"
-    print "\t  Expires: " + EXPIRES
-    my_socket.send(TYPE.upper() + " sip:" + LINE + " SIP/1.0" + "\r\n" +
-    "Expires: " + str(EXPIRES) + "\r\n\r\n")  # Mandamos un solo string
-    data = my_socket.recv(1024)
-
-    print 'Recibido -- ', data
-    print "Terminando socket..."
-
-    # Cerramos todo
-    my_socket.close()
-    print "Fin."
-
-except:
+except IndexError:
     sys.exit('client.py ip puerto register sip_address expires_value')
+except ValueError:
+    sys.exit('client.py ip puerto register sip_address expires_value')
+
+# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((SERVER, PORT))
+
+print "Enviando: " + TYPE.upper() + " sip:" + LINE + " SIP/1.0"
+print "\t  Expires: " + EXPIRES
+my_socket.send(TYPE.upper() + " sip:" + LINE + " SIP/1.0" + "\r\n"
+                   + "Expires: " + str(EXPIRES) + "\r\n\r\n")
+# Mandamos un solo string
+data = my_socket.recv(1024)
+
+print 'Recibido -- ', data
+print "Terminando socket..."
+
+# Cerramos todo
+my_socket.close()
+print "Fin."
