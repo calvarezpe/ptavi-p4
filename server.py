a2dc711f672c0b99ae24fdf77e2da4bfd89338b7
Alberto <alberto@ubuntu.(none)>
2014-10-27 18:23:16 +0100

    ejercicio8 completo sin pep8

--- server.py
+++ server.py
@@ -18,32 +18,43 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     
     def procesarmensaje(self,line):
         mensaje = line.split(" ")
-        
+        fecha_entrada=time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+        hora_actual=time.mktime(time.strptime(fecha_entrada,'%Y-%m-%d %H:%M:%S'))
         if mensaje[0] == "REGISTER":
             direccion= mensaje[1].split(":")
-            lista=[self.client_address[0],time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))]
+            expires=mensaje[-1]
+            lista=[self.client_address[0],time.strftime(fecha_entrada),expires]
             self.diccionario[direccion[-1]]= lista
             self.wfile.write("SIP/2.0 200 OK" + '\r\n' + '\r\n')
             print "El cliente nos manda " + line
-            self.resgister2file()
-            if int(mensaje[-1]) == 0:
+            if int(expires) == 0:
                 del self.diccionario[direccion[-1]]
                 self.wfile.write("SIP/2.0 200 OK" + '\r\n' + '\r\n')
+            self.caducidad(hora_actual)
+        self.register2file()
+        
+    def caducidad(self, hora_actual):
+        claves=self.diccionario.keys()
+        for i in claves:
+            hora_entrada=time.strptime(self.diccionario[i][1],'%Y-%m-%d %H:%M:%S')
+            hora_expiracion=time.mktime(hora_entrada) + float(self.diccionario[i][2])
+            if hora_actual > hora_expiracion:
+                del self.diccionario[i]
+        
 
-    def resgister2file(self):
+    def register2file(self):
         fich = open("registered.txt", "w")
         claves=self.diccionario.keys()
         for i in claves:
-           linea= i + '\t' + self.diccionario[i][0] + '\t' + self.diccionario[i][1]
+           linea= i + '\t' + self.diccionario[i][0] + '\t' + self.diccionario[i][1] +'\r\n'
            fich.write(linea)
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write("Hemos recibido tu peticion" + '\r\n')
-       
+        print "IP: " + self.client_address[0] + " " + "PORT: " + str(self.client_address[1])
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
-            print self.client_address[0] + " " + str(self.client_address[1])
             line = self.rfile.read()
             self.procesarmensaje(line)
             if not line:
