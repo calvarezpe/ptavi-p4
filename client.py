579f077730592f15c23ceec918af60d0fed7d822
abel vargas vera <avera@iota02.aulas.gsyc.es>
2014-10-27 09:54:29 +0100

    Ejercicio 7 terminado

--- client.py
+++ client.py
@@ -12,28 +12,37 @@ listarg = sys.argv
 
 # Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = listarg[1]
-PORT = int(listarg[2])
+try:
+    # Dirección IP del servidor.
+    SERVER = listarg[1]
+    PORT = int(listarg[2])
 
-# Tipo de contenido que vamos a enviar
-TYPE = listarg[3]
+    # Tipo de contenido que vamos a enviar
+    TYPE = listarg[3]
 
-# Contenido que vamos a enviar
-LINE = listarg[4]
+    # Contenido que vamos a enviar
+    LINE = listarg[4]
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+    # Valor de Expires en segundos
+    EXPIRES = listarg[5]
 
-print "Enviando: " + TYPE.upper() + " sip:" + LINE + " SIP/1.0"
-my_socket.send(TYPE.upper() + " sip:" + LINE + " SIP/1.0\r\n\r\n")
-data = my_socket.recv(1024)
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
 
-print 'Recibido -- ', data
-print "Terminando socket..."
+    print "Enviando: " + TYPE.upper() + " sip:" + LINE + " SIP/1.0"
+    print "\t  Expires: " + EXPIRES
+    my_socket.send(TYPE.upper() + " sip:" + LINE + " SIP/1.0" + "\r\n" +
+    "Expires: " + str(EXPIRES) + "\r\n\r\n")  # Mandamos un solo string
+    data = my_socket.recv(1024)
 
-# Cerramos todo
-my_socket.close()
-print "Fin."
+    print 'Recibido -- ', data
+    print "Terminando socket..."
+
+    # Cerramos todo
+    my_socket.close()
+    print "Fin."
+
+except:
+    sys.exit('client.py ip puerto register sip_address expires_value')
