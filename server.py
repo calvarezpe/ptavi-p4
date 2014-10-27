53399326a268e985327bc235ffbc3e7890b59ca5
cristian Alvarez perez <calvarez@iota31.aulas.gsyc.es>
2014-10-27 11:01:02 +0100

    Ejercicio 8 acabado

--- server.py
+++ server.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Clase (y programa principal) para un servidor de eco
+Clase (y programa principal) para un servidor de SIP
 en UDP simple
 """
 
 import SocketServer
 import sys
+import time
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    SIP server class
     """
     direcciones = {}
     
     def register2file(self):
-        fich = open("registered.txt", "r+")
-        if self.expired == "0":
-            print "borrar"
-        elif self.expired > "0":
-            print "añadir"
-    
+        fich = open("registered.txt", "w")
+        line = "User\tIP\tExpires\r\n"
+        for direccion in self.direcciones.keys():
+            hora = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(self.direcciones[direccion][1]))
+            line += direccion + "\t" + self.direcciones[direccion][0] + "\t" + hora + "\r\n"
+        fich.write(line)
+            
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         print self.client_address
@@ -31,23 +33,22 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                 break
             print "El cliente nos manda " + line
             elements = line.split()
-            if elements[0] == "REGISTER":
-                direccion = (elements[1].split(":"))[1];
-                self.expired = elements[-1]
-                if self.expired > "0":
-                    self.direcciones[direccion] = self.client_address[0]
+            direccion = (elements[1].split(":"))[1];
+            expires = elements[-1]
+            if expires > "0":
+                hora = float(expires) + time.time()
+                self.direcciones[direccion] = (self.client_address[0], hora)
+                self.wfile.write("SIP/2.0 200 OK \r\n\r\n")
+            elif expires == "0":
+                if self.direcciones.has_key(direccion):
+                    del self.direcciones[direccion]
                     self.wfile.write("SIP/2.0 200 OK \r\n\r\n")
-                    print "Añadido ", direccion
-                    self.register2file()
-                elif self.expired == "0":
-                    if self.direcciones.has_key(direccion):
-                        del self.direcciones[direccion]
-                        self.wfile.write("SIP/2.0 200 OK \r\n\r\n")
-                        print "Borrado ", direccion
-                        self.register2file() 
-                    else:
-                        print "No encontrado"
-                print self.direcciones
+                else:
+                    self.wfile.write("SIP/2.0 404 NOT FOUND \r\n\r\n")
+            for direccion in self.direcciones.keys():
+                if self.direcciones[direccion][1] < time.time():
+                    del self.direcciones[direccion]
+            self.register2file()
         
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
