dec1bce4344367ec9ef0fb28780a68be5abf343d
cristian Alvarez perez <calvarez@iota32.aulas.gsyc.es>
2014-10-27 13:40:38 +0100

    Servidor corregido pep8

--- server.py
+++ server.py
@@ -9,21 +9,31 @@ import SocketServer
 import sys
 import time
 
+
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     SIP server class
     """
     direcciones = {}
-    
+
     def register2file(self):
+        """
+        Escribe en un fichero la direccion, la ip y la hora limite
+        """
         fich = open("registered.txt", "w")
         line = "User\tIP\tExpires\r\n"
         for direccion in self.direcciones.keys():
-            hora = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(self.direcciones[direccion][1]))
-            line += direccion + "\t" + self.direcciones[direccion][0] + "\t" + hora + "\r\n"
+            hora = time.strftime('%Y-%m-%d %H:%M:%S', \
+time.gmtime(self.direcciones[direccion][1]))
+            line += direccion + "\t" + self.direcciones[direccion][0] + "\t"
+            line += hora + "\r\n"
         fich.write(line)
-            
+
     def handle(self):
+        """
+        Recibe mensajes register y almacena en un diccionario la direccion,
+        la ip y la hora limite
+        """
         # Escribe dirección y puerto del cliente (de tupla client_address)
         print self.client_address
         while 1:
@@ -33,14 +43,14 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                 break
             print "El cliente nos manda " + line
             elements = line.split()
-            direccion = (elements[1].split(":"))[1];
+            direccion = (elements[1].split(":"))[1]
             expires = elements[-1]
             if expires > "0":
                 hora = float(expires) + time.time()
                 self.direcciones[direccion] = (self.client_address[0], hora)
                 self.wfile.write("SIP/2.0 200 OK \r\n\r\n")
             elif expires == "0":
-                if self.direcciones.has_key(direccion):
+                if direccion in self.direcciones:
                     del self.direcciones[direccion]
                     self.wfile.write("SIP/2.0 200 OK \r\n\r\n")
                 else:
@@ -49,9 +59,9 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                 if self.direcciones[direccion][1] < time.time():
                     del self.direcciones[direccion]
             self.register2file()
-        
+
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
+    # Creamos servidor de sip y escuchamos
     serv = SocketServer.UDPServer(("", int(sys.argv[1])), SIPRegisterHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
